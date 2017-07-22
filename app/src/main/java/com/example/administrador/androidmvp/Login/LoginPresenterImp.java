b18d843af991918dbac649ca5cc1f1adc280ce4f
OmarGtz <omarGtz1608@gmail.com>
2017-07-21 19:26:09 -0500

    mvp parcial

--- app/src/main/java/com/example/administrador/androidmvp/Login/LoginPresenterImp.java
+++ app/src/main/java/com/example/administrador/androidmvp/Login/LoginPresenterImp.java
@@ -9,25 +9,21 @@ public class LoginPresenterImp implements LoginPresenter, LoginInteractor.onLogi
     LoginView loginView;
     LoginInteractor loginInteractor;
 
-
     LoginPresenterImp(LoginView view){
         this.loginView = view;
         this.loginInteractor = new LoginInteractorImpl();
     }
-
 
     @Override
     public void ValidCredentials(String mail, String pass) {
 
         loginInteractor.Login(mail,pass,this);
 
-
     }
 
     @Override
     public void onErrorUserLogin() {
         loginView.setUserNameError();
-
     }
 
     @Override
